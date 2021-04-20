0f9fc406525e79309d6c389646e1c6c2501a45a6
Ruan Miguel <ruanmiguelps@gmail.com>
2021-04-20 17:21:41 -0300

    fix

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,6 @@
 SELECT UCASE( CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS Nome_completo,
-E.HIRE_DATE AS 'Data de início',
+H.START_DATE AS 'Data de início',
 E.SALARY AS 'Salário'
 FROM hr.employees AS E
+    hr.job_history AS H ON E.JOB_ID = H.JOB_ID
 ORDER BY Nome_completo ASC, E.HIRE_DATE;
---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT 
+    UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS Nome_completo,
+    H.START_DATE AS 'Data de início',
+    E.SALARY AS 'Salário'
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS H ON E.JOB_ID = H.JOB_ID
+WHERE
+    MONTH(H.START_DATE) IN (1 , 2, 3)
+ORDER BY Nome_completo ASC , H.START_DATE;
+
